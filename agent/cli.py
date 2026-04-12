4272e70ad7b803c08d82aabb725e4002cad5e2fc
lxhcaicai <2778763221@qq.com>
2026-04-12 18:17:57 +0800

    fix: add sqlite check_point

--- agent/cli.py
+++ agent/cli.py
@@ -1,29 +1,33 @@
 import uuid
 
+from langgraph.checkpoint.sqlite import SqliteSaver
+
 from agent import server
 
 def run():
-    config = {"configurable": {"thread_id": str(uuid.uuid4())  }}
-    agent = server.get_agent()
-    while True:
-        content = input("> ")
-        if not content.strip():
-            continue
+    config = {"configurable": {"thread_id": "1234" }}
+    with SqliteSaver.from_conn_string("../cai-coder-sqlite.db") as checkpointer:
+        agent = server.get_agent(checkpointer)
 
-        if content.strip().lower() == "exit":
-            break
+        while True:
+            content = input("> ")
+            if not content.strip():
+                continue
 
-        for chunk in agent.stream(
-                {"messages": [{"role": "user", "content": f"{content}"}]},
-                stream_mode=["messages"],
-                version="v2",
-                config= config
-        ):
-            if chunk["type"] == "messages":
-                token, metadata = chunk["data"]
-                print(token.content, end='', flush=True)
+            if content.strip().lower() == "exit":
+                break
 
-        print("\n")
+            for chunk in agent.stream(
+                    {"messages": [{"role": "user", "content": f"{content}"}]},
+                    stream_mode=["messages"],
+                    version="v2",
+                    config= config
+            ):
+                if chunk["type"] == "messages":
+                    token, metadata = chunk["data"]
+                    print(token.content, end='', flush=True)
+
+            print("\n")
 
 if __name__ == "__main__":
     run()
